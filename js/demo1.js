09545e760912681f9eb83783ba88791c7bab6c1b
lawlietfeng <531672245@qq.com>
2017-03-01 22:31:48 +0800

    简单的JS实现换肤

---
+++ js/demo1.js
@@ -0,0 +1,15 @@
+/**
+ * Created by 老婆 on 2017/3/1.
+ */
+function tob() {
+    var a = document.getElementById('l1');
+    a.href = "css/demo1.1.css";
+}
+function tow() {
+    var a = document.getElementById('l1');
+    a.href = "css/demo1.2.css";
+}
+function tog() {
+    var a = document.getElementById('l1');
+    a.href = "css/demo1.3.css";
+}
